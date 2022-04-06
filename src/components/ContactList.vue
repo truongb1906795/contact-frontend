083f826f73ed43454d4e518a84de9ed7021ac6cc
Le Van Truong <truongb1906795@student.ctu.edu.vn>
2022-04-06 16:39:07 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/ContactList.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    props: {
+        contacts: { type: Array, default: [] },
+        activeIndex: { type: Number, default: -1 },
+    },
+    emits: ["update:activeIndex"],
+    watch: {
+        activeIndex(newValue) {
+            if (newValue === -1) {
+                this.selectedIndex = -1;
+            }
+        },
+    },
+    data() {
+        return {
+            selectedIndex: this.activeIndex,
+        };
+    },
+    methods: {
+        select(index) {
+            this.selectedIndex = index;
+            this.$emit("update:activeIndex", index);
+        },
+    },
+};
+</script>
+
+<template>
+    <ul class="list-group">
+        <li
+            class="list-group-item"
+            v-for="(contact, index) in contacts"
+            :key="contact.id"
+            :class="{ active: index === selectedIndex }"
+            @click="select(index)"
+        >
+            {{ contact.name }}
+        </li>
+    </ul>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "ContactBook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "EditContact",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+    {
+        path:"/addContact",
+        name:"AddContact",
+        component: () => import("@/views/AddContact.vue"),
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
+
+export default router;
